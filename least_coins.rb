06c69ff853d19f95dddc5909c07cecf03ff1d5ac
madisonparker <christaandmadison@gmail.com>
2018-07-27 17:26:25 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,22 @@
-#write out your code here
-
 def least_coins(cents)
-
-#Code your answer here!
-
+coins = {}
+coins["quarter"] = 25
+coins["dime"] = 10 
+coins["nickel"] = 25
+coins["penny"] = 1 
+quarters = 0 
+dimes = 0 
+nickel = 0 
+penny = 0 
+until cents < 25
+nickel += 1 
+cents = cents - 25
 end
+until cents < 10
+dime += 1
+cents = cents - 10
+end
+until cents < 5 
+nickel += 1
+cents = cents - 5
+end 
